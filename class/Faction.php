52e2100878014893fb94f23ff896e7ff6d7b8ba6
Yann Martin <ymartin@la-providence.net>
2021-04-27 17:42:48 +0200

    Mise en form

---
+++ class/Faction.php
@@ -0,0 +1,58 @@
+<?php
+//cette classe est développé par : Yann  Martin
+
+class Faction extends Personnange {
+
+    /**
+     * 
+     * Cette fonction permet modifier le nom de la faction
+     * 
+     * Entries :
+     * 
+     * $nom = variable contenant le nom de la faction
+    */
+    public function setNom($nom) {
+
+
+    }
+
+    /**
+     * 
+     * Cette fonction permet modifier la couleur de la faction
+     * 
+     * Entries :
+     * 
+     * $couleur = variable contenant la couleur
+    */
+    public function setColor($couleur) {
+        
+
+    }
+
+    /**
+     * 
+     * Cette fonction permet de supprimer une faction
+     * 
+     * Entries :
+     * 
+     * $id = variable contenant l'id de la faction
+    */
+    public function delFaction($id) {
+        
+
+    }
+
+    /**
+     * 
+     * Cette fonction permet de supprimer une faction
+     * 
+     * Entries :
+     * 
+     * $id = variable contenant l'id de la faction
+    */
+    public function setFaction($id) {
+        
+
+    }
+
+}
